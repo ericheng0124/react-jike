425e667ee80a1622c19febc54c6aa19f07d9ecab
ericheng0124 <233347237@qq.com>
2024-05-11 15:09:51 +0800

    封装图表模块

---
+++ src/pages/Home/components/barChart.js
@@ -0,0 +1,40 @@
+// 柱状图组件
+import {useEffect, useRef} from "react"
+import * as echarts from "echarts"
+
+const BarChart = ({title}) => {
+  const chartRef = useRef(null)
+  useEffect(() => {
+    // 保证dom可用,才可以进行图表渲染
+    // 1. 获取渲染图表的dom节点
+    const chartDom = chartRef.current
+    // 2. 图表初始化生成图表实例对象
+    const myChart = echarts.init(chartDom)
+    // 3. 准备图表参数
+    const option = {
+      title: {
+        text: title
+      },
+      xAxis: {
+        type: 'category',
+        data: ['Angular', 'Vue', 'React']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [10, 40, 70],
+          type: 'bar'
+        }
+      ]
+    }
+    // 3. 使用图表参数完成图表渲染
+    option && myChart.setOption(option)
+  }, [])
+  return (
+    <div ref={chartRef} style={{height: 300, width: 400}}></div>
+  )
+}
+
+export default BarChart
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,38 +1,10 @@
-import * as echarts from 'echarts'
-import {useEffect, useRef} from "react";
-
+import React from "react"
+import BarChart from "@/pages/Home/components/barChart";
 
 const Home = () => {
-  const chartRef = useRef(null)
-  useEffect(() => {
-    // 保证dom可用,才可以进行图表渲染
-    // 1. 获取渲染图表的dom节点
-    const chartDom = chartRef.current
-    // 2. 图表初始化生成图表实例对象
-    const myChart = echarts.init(chartDom)
-    // 3. 准备图表参数
-    const option = {
-      xAxis: {
-        type: 'category',
-        data: ['Angular','Vue','React']
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [
-        {
-          data: [10,40,70],
-          type: 'bar'
-        }
-      ]
-    }
-    // 3. 使用图表参数完成图表渲染
-    option && myChart.setOption(option)
-  }, [])
-
   return (
     <div>
-      <div ref={chartRef} style={{height: 400, width: 500}}></div>
+      <BarChart title={'三大框架满意度'}/><BarChart title={'三大框架使用度'}/>
     </div>
   );
 };
