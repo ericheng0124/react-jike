c7b92c0793ecd0db289fefbfb4dfef64c546a67b
ericheng0124 <233347237@qq.com>
2024-05-22 16:03:09 +0800

    打包优化-配置路由懒加载+打包体积分析

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,45 @@
-import {createBrowserRouter} from "react-router-dom";
-import Layout from "@/pages/Layout";
-import Login from "@/pages/Login";
-import {AuthRoute} from "@/components/AuthRoute";
-import Home from "@/pages/Home";
-import Article from "@/pages/Article";
-import Publish from "@/pages/Publish";
+import {createBrowserRouter} from "react-router-dom"
+import Layout from "@/pages/Layout"
+import Login from "@/pages/Login"
+import {AuthRoute} from "@/components/AuthRoute"
+import {lazy, Suspense} from "react";
+// import Home from "@/pages/Home"
+// import Article from "@/pages/Article"
+// import Publish from "@/pages/Publish"
+
+
+const Publish = lazy(() => import('@/pages/Publish'))
+const Article = lazy(() => import('@/pages/Article'))
+const Home = lazy(() => import('@/pages/Home'))
+
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <AuthRoute><Layout/></AuthRoute>,
-    children:[
+    children: [
       {
-        index:true,  // 将home作为二级路由默认显示
-        element:<Home/>
+        index: true,  // 将home作为二级路由默认显示
+        element: (
+          <Suspense fallback={'加载中'}>
+            <Home/>
+          </Suspense>
+        )
       },
       {
-        path:'article',
-        element:<Article/>
-      },{
-        path:'publish',
-        element:<Publish/>
+        path: 'article',
+        element: (
+          <Suspense fallback={'加载中'}>
+            <Article/>
+          </Suspense>
+        )
+      }, {
+        path: 'publish',
+        element: (
+          <Suspense fallback={'加载中'}>
+            <Publish/>
+          </Suspense>
+        )
       },
     ]
   },
